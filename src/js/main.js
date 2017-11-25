4f3e6c3dbcc1fbfe44502f98ac832cc19ea74017
Sergey Khmelevskoy <xs290@me.com>
2017-11-25 20:15:24 +0300

    added popups as separate pages

--- src/js/main.js
+++ src/js/main.js
@@ -79,18 +79,20 @@ $(document).ready(function(){
   // HORIZONTAL SCROLLER
   //////////
 
-  $.jInvertScroll([
-    '.scroll'
-  ], {
-  	width: 'auto',	// Page width (auto or int value)
-  	height: 'auto',	// Page height (the shorter, the faster the scroll)
-  	onScroll: function(percent) {
-  		// Callback function that will be called each time the user
-  		// scrolls up or down, useful for animating other parts
-  		// on the page depending on how far the user has scrolled down
-  		// values go from 0.0 to 1.0 (with 4 decimals precision)
-	   }
-  });
+  if( $('.homepage').length > 0 ){
+    $.jInvertScroll([
+      '.scroll'
+    ], {
+    	width: 'auto',	// Page width (auto or int value)
+    	height: 'auto',	// Page height (the shorter, the faster the scroll)
+    	onScroll: function(percent) {
+    		// Callback function that will be called each time the user
+    		// scrolls up or down, useful for animating other parts
+    		// on the page depending on how far the user has scrolled down
+    		// values go from 0.0 to 1.0 (with 4 decimals precision)
+  	   }
+    });  
+  }
 
 
 
